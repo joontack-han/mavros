dc368d113184b733e0cb7578e91ca4a9820a49a4
Sangyul Park <sangyul@snu.ac.kr>
2018-08-21 14:51:27 +0900

    cov meassage changed to geometry_msgs type

--- mavros_extras/src/plugins/estimator_status.cpp
+++ mavros_extras/src/plugins/estimator_status.cpp
@@ -1,7 +1,7 @@
 #include <mavros/mavros_plugin.h>
 #include <pluginlib/class_list_macros.h>
-
-#include <mavros_msgs/EstimatorStatus.h>
+#include <geometry_msgs/PoseStamped.h>
+//#include <mavros_msgs/EstimatorStatus.h>
 
 namespace mavplugin {
 /**
@@ -19,7 +19,7 @@ public:
 	void initialize(UAS &uas_)
 	{
  		uas = &uas_;
-		estimator_status_pub = nh.advertise<mavros_msgs::EstimatorStatus>("est_status", 10);
+		estimator_status_pub = nh.advertise<geometry_msgs::PoseStamped>("est_status", 10);
 	}
 
 	const message_map get_rx_handlers() {
@@ -39,7 +39,7 @@ private:
 		mavlink_estimator_status_t estimator_status;
 		mavlink_msg_estimator_status_decode(msg, &estimator_status);
 
-		auto ros_msg = boost::make_shared<mavros_msgs::EstimatorStatus>();
+		auto ros_msg = boost::make_shared<geometry_msgs::PoseStamped>();
 
 		uint32_t sec, ns;
 		sec = estimator_status.time_usec/1000000;
@@ -47,13 +47,14 @@ private:
 		ros_msg->header.stamp = ros::Time(sec, ns);
 
 		//ros_msg->covariances[i] = estimator_status.covariances[i];
-		ros_msg->cov_qw = estimator_status.cov_qw;
-		ros_msg->cov_qx = estimator_status.cov_qx;
-		ros_msg->cov_qy = estimator_status.cov_qy;
-		ros_msg->cov_qz = estimator_status.cov_qz;
-		ros_msg->cov_px = estimator_status.cov_px;
-		ros_msg->cov_py = estimator_status.cov_py;
-		ros_msg->cov_pz = estimator_status.cov_pz;
+
+		ros_msg->pose.position.x = estimator_status.cov_px;
+		ros_msg->pose.position.y = estimator_status.cov_py;
+		ros_msg->pose.position.z = estimator_status.cov_pz;
+		ros_msg->pose.orientation.w = estimator_status.cov_qw;
+		ros_msg->pose.orientation.x = estimator_status.cov_qx;
+		ros_msg->pose.orientation.y = estimator_status.cov_qy;
+		ros_msg->pose.orientation.z = estimator_status.cov_qz;
 
 		
 		estimator_status_pub.publish(ros_msg);
